236197f662e58639af68740b9400a72f3bba33ca
jagadish <jagadish.gowda@redingtongroup.com>
2023-10-03 17:37:14 +0530

    modified as github hosted runner

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const ENV = 'DEV';
 const app = express();
 app.get('/', (req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello from Redington Devops Team, this was awesome added a github runner with self-hosted';
+  const msg = 'Hello from Redington Devops Team, this was awesome';
   res.send(getPage(msg));
 });
 
